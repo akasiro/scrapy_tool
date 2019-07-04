35f0e18fd7f4435c40bd00cfbe3a5427e6f3c78e
akasiro <ruc_chenping@outlook.com>
2019-07-05 06:06:40 +0800

    fix bug, add Readme.md

--- scrapy_tool.py
+++ scrapy_tool.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
-import requests, time
-import random, json
+import requests, time, json
+import random
 from bs4 import BeautifulSoup
 
 class scrapy_tool():
-    def __init__(self):
+    def __init__(self, test_url = 'http://www.baidu.com'):
         self.Default_headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 
         with open('user_agent','r') as f:
             self.list_user_agent = f.readlines()
 
+        self.test_url = test_url
         self.ip_list = list(set(self.get_ip_list1()+ self.get_ip_list2()))
+        self.proxies = None
 
+
+    #获取随机header
     def random_headers(self):
         headers = {
             'User-Agent': random.choice(self.list_user_agent).replace('\n','')
         }
         return headers
 
-    def random_proxy(self, test_url = 'http://www.baidu.com'):
-        proxies = None
+    #获取随机代理ip
+    def random_proxy(self):
         while True:
             if len(self.ip_list) == 0:
                 break
@@ -30,20 +34,20 @@ class scrapy_tool():
                 'https': 'https://{}'.format(ip_temp)
             }
             try:
-                res = requests.get(test_url,headers = self.Default_headers, proxies = proxies, timeout = 2)
+                res = requests.get(self.test_url,headers = self.Default_headers, proxies = proxies_temp, timeout = 1)
                 if res.status_code == 200:
                     res.close()
-                    proxies = proxies_temp
+                    self.proxies = proxies_temp
                     break
                 else:
                     res.close()
-                    self.ip_list.remove(proxies_temp)
+                    self.ip_list.remove(ip_temp)
             except:
-                self.ip_list.remove(proxies_temp)
+                self.ip_list.remove(ip_temp)
                 time.sleep(0.5)
-        return proxies
+        return self.proxies
 
-    #使用qiye的IProxy程序获取ip
+    #使用qiye的IProxy程序获取ip池
     def get_ip_list1(self):
         ip_list = []
         try:
@@ -55,14 +59,14 @@ class scrapy_tool():
             ip_list = []
         return ip_list
 
-    #直接爬取获取ip
+    #直接爬取获取ip池
     def get_ip_list2(self):
         url = 'http://www.xicidaili.com/wn/'
 
         ip_list = []
         try:
             web_data = requests.get(url, headers=self.Default_headers)
-            soup = BeautifulSoup(web_data.text, 'lxml')
+            soup = BeautifulSoup(web_data.text, 'html.parser')
             ips = soup.find_all('tr')
             ip_list = []
             for i in range(1, len(ips)):
@@ -72,6 +76,15 @@ class scrapy_tool():
         except:
             pass
         return ip_list
+    #刷新ip池
+    def refresh_ip_pool(self):
+        self.ip_list = list(set(self.get_ip_list1() + self.get_ip_list2()))
+    #换代理ip
+    def change_proxy(self):
+        if self.proxies != None:
+            self.ip_list.remove(self.proxies['http'].replace('http://',''))
+        return self.random_proxy()
+
 
 
 
@@ -79,6 +92,7 @@ class scrapy_tool():
 
 if __name__ == '__main__':
     url = 'http://aso.niaogebiji.com'
-    st = scrapy_tool()
+    st = scrapy_tool(url)
     print(st.random_headers())
     print(st.random_proxy())
+    print(st.change_proxy())
